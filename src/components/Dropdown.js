4607b9955baeb394277529ef9c74e3870d71825c
woengus <olivierbiard@woengus.com>
2023-02-15 17:24:23 +0100

    css fini

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,24 +4,45 @@ import icon2 from "../assets/down_arrow.svg";
 
 const Dropdown = (props) => {
   const [open, setOpen] = useState(false);
-  return (
-    <div className="dropdown-menu">
-      <div className="dropdown-title">
-        <h2>{props.title}</h2>
-        <img
-          src={icon2}
-          alt="flêche ouverture"
-          className="icon"
-          onClick={() => setOpen(!open)}
-        />
-      </div>
-      {open ? (
-        <div className="dropdown-text">
-          <p>{props.text}</p>
+  if (open === true) {
+    return (
+      <div className="dropdown-menu">
+        <div className="dropdown-title">
+          <h2>{props.title}</h2>
+          <img
+            src={icon}
+            alt="flêche fermeture"
+            className="icon"
+            onClick={() => setOpen(!open)}
+          />
         </div>
-      ) : null}
-    </div>
-  );
+        {open ? (
+          <div className="dropdown-text">
+            <p>{props.text}</p>
+          </div>
+        ) : null}
+      </div>
+    );
+  } else {
+    return (
+      <div className="dropdown-menu">
+        <div className="dropdown-title">
+          <h2>{props.title}</h2>
+          <img
+            src={icon2}
+            alt="flêche ouverture"
+            className="icon"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+        {open ? (
+          <div className="dropdown-text">
+            <p>{props.text}</p>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 };
 
 export default Dropdown;
